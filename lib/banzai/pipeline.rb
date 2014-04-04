af6761b5fe6c136a38829e5ccd051119ef0489b2
Carlos Rodriguez <carlos@eddorre.com>
2014-04-04 14:40:16 -0700

    Initial commit. Added .ruby-version and .rbenv-gemsets to .gitignore file.

---
+++ lib/banzai/pipeline.rb
@@ -0,0 +1,7 @@
+require "banzai/pipeline/version"
+
+module Banzai
+  module Pipeline
+    # Your code goes here...
+  end
+end
---
+++ lib/banzai/pipeline/version.rb
@@ -0,0 +1,5 @@
+module Banzai
+  module Pipeline
+    VERSION = "0.0.1"
+  end
+end
